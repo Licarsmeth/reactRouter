9fa78c4475613559f63bb8c6258afd154bf56a2f
Licar <84367661+Licarsmeth@users.noreply.github.com>
2024-04-21 13:47:03 +0545

    error handling using useRouteError

--- src/components/ErrorPage.jsx
+++ src/components/ErrorPage.jsx
@@ -1,9 +1,16 @@
 import { Link } from "react-router-dom";
+import { useRouteError } from "react-router-dom";
 
 const ErrorPage = () => {
+  const error = useRouteError();
+  console.error(error);
+
   return (
     <div>
       <h1>Oh no, this route doesnt exist!</h1>
+      <p>
+        <i>{error.statusText || error.message}</i>
+      </p>
       <Link to="/">
         You can go back to the home page by clicking here, though!
       </Link>
